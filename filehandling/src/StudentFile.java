d220d30db356467cd568128fd6c085266eb16fa6
deeksha2422 <deeksha18csu057@ncuindia.edu>
2019-02-04 17:49:16 +0530

    update

--- filehandling/src/StudentFile.java
+++ filehandling/src/StudentFile.java
@@ -1,20 +1,115 @@
 import java.io.*;
 import java.util.*;
-class StudentFile{
-	public static void main(String args[]){
-		File F = null;
-		Scanner in = new Scanner(System.in);
-		try{
-			Student s= new Student();
-			F=new File("D:\\Coding\\Java_Programs\\FileHandling\\files\\Student.txt");
-			FileWriter Fout=new FileWriter(F);
-			BufferedWriter bout = new BufferedWriter(Fout);
-			while(in.hasNext()){
-				bout.write(s.Array());
-			}
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-	}
+class Student {
+    int sNo;
+    String fName;
+    float cgpa;
+    char grade;
+    Scanner input = new Scanner(System.in);
+
+    void setStudent() {
+        System.out.print("Enter Serial No : ");
+        sNo = input.nextInt();
+        System.out.print("Enter First Name : ");
+        fName = input.next();
+        System.out.print("Enter CGPA : ");
+        cgpa = input.nextFloat();
+        System.out.print("Enter grade : ");
+        grade = input.next().charAt(0);
+    }
+
+    public String toString() {
+        return sNo + "\t" + fName + "\t" + cgpa + "\t" + grade + "\n"; 
+    }
+
+    void getStudent() {
+        System.out.print("Serial No : " + sNo);
+        System.out.print("First Name : " + fName);
+        System.out.print("CGPA : " + cgpa);
+        System.out.print("garde : " + grade);
+    }
+}
+class Program1 {
+
+    static void input() {
+        File f = null;
+        try {
+            f = new File("C:\\College\\Java_FOCPII\\FileHandling\\files\\student.txt");
+            FileWriter fout = new FileWriter(f);
+            BufferedWriter bout= new BufferedWriter(fout);
+            Student s[] = new Student[5];
+            for (int i=0; i<5; i++) {
+                s[i] = new Student();
+                s[i].setStudent();
+                bout.write(s[i].toString());
+                bout.newLine();
+            }
+            
+            bout.close();
+        }        
+        catch (Exception e) {
+        // if any I/O error occurs
+            e.printStackTrace();
+        }
+    }
+
+    static void output() {
+        File f=null;
+        try {
+            f = new File("C:\\College\\Java_FOCPII\\FileHandling\\files\\student.txt");
+            FileReader fin = new FileReader(f);
+            BufferedReader bin= new BufferedReader(fin);
+            String sr;
+            System.out.println("The contents of the file are: ");
+            System.out.println("S.No\tName\tCGPA\tgrade");
+            while((sr=bin.readLine())!=null){
+                System.out.println(sr);
+            } 
+        }
+        catch (Exception e) {
+        // if any I/O error occurs
+            e.printStackTrace();
+        }
+        
+    }
+
+    static void search() {
+        File f = null;
+        try {
+            boolean flag=true;
+            FileReader fin = new FileReader(f);
+            String s;
+            BufferedReader bin= new BufferedReader(fin);
+                while ((s=bin.readLine())!=null && flag) {
+                    if(s.equals("me")) {
+                        System.out.print("Present");
+                    flag=false;
+                }
+
+            } 
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char ch;
+        char c;
+        do {
+            ch = input.next().charAt(0);
+            switch(ch) {
+                case 'i' : input();
+                        break;
+                case 'o' : output();
+                        break;
+                case 's' : search();
+                        break;
+                default : System.out.println("INVALID");
+            }
+           
+            c = input.next().charAt(0);
+        } while (c=='y');
+        
+    }
 }
